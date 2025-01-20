1547198854855393926faea5db5b9e0e38850a7a
Muhammad Ramdhan <evc.adhan021@gmail.com>
2025-01-20 07:39:21 +0700

    membuat layout login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
   const Login({super.key});
 
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,9 +15,35 @@ class Login extends StatelessWidget {
         width: double.infinity,
         decoration: const BoxDecoration(
             image: DecorationImage(
-          image: AssetImage("assets/add_page/background-.jpg"),
+          image: AssetImage("assets/add_image/background-.jpg"),
           fit: BoxFit.cover,
         )),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            Image.asset(
+              'assets/add_image/flutter.png',
+              scale: 5,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(60),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
