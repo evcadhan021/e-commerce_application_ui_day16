412b4c0ebd5b4325a9bb7416d5ea5beed2147e22
Muhammad Ramdhan <evc.adhan021@gmail.com>
2025-01-21 09:33:01 +0700

    hampir menyelesaikan screen login

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -8,6 +8,7 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool _sensorPassword = true; // untuk toggle sensor password
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +20,7 @@ class _LoginState extends State<Login> {
           fit: BoxFit.cover,
         )),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          // crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const SizedBox(
               height: 100,
@@ -30,14 +31,109 @@ class _LoginState extends State<Login> {
               color: Colors.white,
             ),
             const SizedBox(
-              height: 80,
+              height: 100,
             ),
             Expanded(
               child: Container(
+                width: double.infinity,
                 decoration: const BoxDecoration(
-                  color: Colors.white,
+                  color: Colors.white38,
                   borderRadius: BorderRadius.only(
                     topLeft: Radius.circular(60),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      const Text(
+                        "E-commerce \n rmmdh.n",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Nunito',
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                          height: 1.2,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const SizedBox(height: 40),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "Email",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Container(
+                            height: 60,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: const TextField(
+                              decoration: InputDecoration(
+                                hintText: "Masukkan email anda",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none,
+                                contentPadding: EdgeInsets.symmetric(
+                                    vertical: 18.0,
+                                    horizontal: 12.0), // Atur padding
+                              ),
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 20,
+                          ),
+                          const Text(
+                            "Password",
+                            style: TextStyle(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Container(
+                            height: 60,
+                            width: 300,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: TextField(
+                              obscureText: _sensorPassword,
+                              decoration: InputDecoration(
+                                  hintText: "Masukkan password anda",
+                                  hintStyle:
+                                      const TextStyle(color: Colors.grey),
+                                  border: InputBorder.none,
+                                  contentPadding: const EdgeInsets.symmetric(
+                                      vertical: 18.0,
+                                      horizontal: 12.0), // Atur padding
+                                  suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _sensorPassword =
+                                              !_sensorPassword; // toggle sensor disini
+                                        });
+                                      },
+                                      icon: Icon(
+                                        _sensorPassword
+                                            ? Icons.visibility_off
+                                            : Icons.visibility,
+                                        color: Colors.grey,
+                                      ))),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
                   ),
                 ),
               ),
